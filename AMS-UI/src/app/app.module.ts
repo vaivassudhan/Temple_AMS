2a3078dde0288a3302363a03e352c32715914a9a
vaivassudhan <vaivassaravanan1@gmail.com>
2021-01-28 21:49:23 +0530

    printer

--- AMS-UI/src/app/app.module.ts
+++ AMS-UI/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {NgxPrinterModule} from 'ngx-printer';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPrinterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
